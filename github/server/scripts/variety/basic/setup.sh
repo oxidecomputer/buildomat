3d2c8671df83314fb1ab22e91dffa6200e31d106
Joshua M. Clulow <jmc@oxide.computer>
2024-10-24 15:33:07 -0700

    XXX github: basic variety unprivileged support

--- github/server/scripts/variety/basic/setup.sh
+++ github/server/scripts/variety/basic/setup.sh
@@ -8,6 +8,14 @@ kern="$(uname -s)"
 
 case "$kern" in
 SunOS)
+	if [[ $UID == 12345 ]]; then
+		#
+		# Running unprivileged.  Setup was already done by the factory.
+		#
+		printf 'INFO: running unprivileged!\n'
+		exit 0
+	fi
+
 	groupadd -g 12345 build
 	useradd -u 12345 -g build -d /home/build -s /bin/bash \
 	    -c 'build' -P 'Primary Administrator' build
