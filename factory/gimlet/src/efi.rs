83769b9e9ab70678d53e5549e63c9199b1caf050
Joshua M. Clulow <jmc@oxide.computer>
2025-05-23 20:56:33 -0700

    XXX clean out disks and add swap etc

---
+++ factory/gimlet/src/efi.rs
@@ -0,0 +1,198 @@
+use std::os::fd::{AsRawFd, RawFd};
+
+use anyhow::{bail, Result};
+
+#[allow(non_camel_case_types)]
+#[allow(unused)]
+pub mod sys {
+    use libc::{c_char, c_int, c_uint, c_ushort};
+
+    pub type diskaddr_t = libc::c_ulonglong;
+
+    /*
+     * Partition identification tags
+     */
+    pub const V_UNASSIGNED: c_ushort = 0x00;
+    pub const V_BOOT: c_ushort = 0x01;
+    pub const V_ROOT: c_ushort = 0x02;
+    pub const V_SWAP: c_ushort = 0x03;
+    pub const V_USR: c_ushort = 0x04;
+    pub const V_BACKUP: c_ushort = 0x05;
+    pub const V_STAND: c_ushort = 0x06;
+    pub const V_VAR: c_ushort = 0x07;
+    pub const V_HOME: c_ushort = 0x08;
+    pub const V_ALTSCTR: c_ushort = 0x09;
+    pub const V_CACHE: c_ushort = 0x0a;
+    pub const V_RESERVED: c_ushort = 0x0b;
+    pub const V_SYSTEM: c_ushort = 0x0c;
+    pub const V_VXVM_PUB: c_ushort = 0x0e;
+    pub const V_VXVM_PRIV: c_ushort = 0x0f;
+    pub const V_BIOS_BOOT: c_ushort = 0x18;
+    pub const V_NETBSD_FFS: c_ushort = 0xff;
+
+    pub const EFI_MIN_ARRAY_SIZE: c_uint = 16 * 1024;
+
+    #[derive(Debug, Clone)]
+    #[repr(C)]
+    pub struct uuid {
+        pub time_low: u32,
+        pub time_mid: u16,
+        pub time_hi_and_version: u16,
+        pub clock_seq_hi_and_reserved: u8,
+        pub clock_seq_low: u8,
+        pub node_addr: [u8; 6],
+    }
+
+    #[derive(Debug, Clone)]
+    #[repr(C)]
+    pub struct dk_part {
+        pub p_start: diskaddr_t,
+        pub p_size: diskaddr_t,
+        pub p_guid: uuid,
+        pub p_tag: c_ushort,
+        pub p_flag: c_ushort,
+        pub p_name: [c_char; 36],
+        pub p_uguid: uuid,
+        pub p_resv: [c_uint; 8],
+    }
+
+    #[repr(C)]
+    pub struct dk_gpt_t {
+        /*** Set to EFI_VERSION_CURRENT */
+        pub efi_version: c_uint,
+        /*** number of partitions */
+        pub efi_nparts: c_uint,
+        /*** size of each entry (unused?!) */
+        pub efi_part_size: c_uint,
+        /*** size of block in bytes */
+        pub efi_lbasize: c_uint,
+        /*** last block on disk */
+        pub efi_last_lba: diskaddr_t,
+        /*** first block after labels */
+        pub efi_first_u_lba: diskaddr_t,
+        /*** last block before backup labels */
+        pub efi_last_u_lba: diskaddr_t,
+        pub efi_disk_uguid: uuid,
+        pub efi_flags: c_uint,
+        pub efi_reserved1: c_uint,
+        pub efi_altern_lba: diskaddr_t,
+        pub efi_reserved: [c_uint; 12],
+        pub efi_parts: [dk_part; 1],
+    }
+
+    #[link(name = "efi")]
+    unsafe extern "C" {
+        pub fn efi_alloc_and_init(
+            fd: c_int,
+            nparts: u32,
+            vtoc: *mut *mut dk_gpt_t,
+        ) -> c_int;
+        pub fn efi_alloc_and_read(fd: c_int, vtoc: *mut *mut dk_gpt_t)
+            -> c_int;
+        pub fn efi_write(fd: c_int, vtoc: *mut dk_gpt_t) -> c_int;
+        pub fn efi_free(vtoc: *mut dk_gpt_t);
+        pub fn efi_reserved_sectors(vtoc: *mut dk_gpt_t) -> c_uint;
+    }
+}
+
+pub struct Gpt {
+    vtoc: *mut sys::dk_gpt_t,
+}
+
+impl Drop for Gpt {
+    fn drop(&mut self) {
+        unsafe { sys::efi_free(self.vtoc) };
+    }
+}
+
+impl Gpt {
+    pub fn init_from_fd(f: RawFd) -> Result<Gpt> {
+        let fd = f.as_raw_fd();
+
+        let mut vtoc = std::ptr::null_mut();
+        let r = unsafe { sys::efi_alloc_and_init(fd, 9, &mut vtoc) };
+        if r < 0 {
+            bail!("efi_alloc_and_init(3EXT) failed with {r}");
+        }
+        if vtoc.is_null() {
+            bail!("NULL vtoc?!");
+        }
+
+        Ok(Gpt { vtoc })
+    }
+
+    pub fn read_from_fd(f: RawFd) -> Result<Gpt> {
+        let fd = f.as_raw_fd();
+
+        let mut vtoc = std::ptr::null_mut();
+        let r = unsafe { sys::efi_alloc_and_read(fd, &mut vtoc) };
+        if r < 0 {
+            bail!("efi_alloc_and_read(3EXT) failed with {r}");
+        }
+        if vtoc.is_null() {
+            bail!("NULL vtoc?!");
+        }
+
+        Ok(Gpt { vtoc })
+    }
+
+    pub fn write_to_fd(&mut self, f: RawFd) -> Result<()> {
+        let fd = f.as_raw_fd();
+
+        let r = unsafe { sys::efi_write(fd, self.vtoc) };
+        if r < 0 {
+            bail!("efi_write(3EXT) failed with {r}");
+        }
+
+        Ok(())
+    }
+
+    pub fn block_size(&self) -> u64 {
+        u64::from(unsafe { (*self.vtoc).efi_lbasize })
+    }
+
+    pub fn reserved_sectors(&self) -> u32 {
+        unsafe { sys::efi_reserved_sectors(self.vtoc) }
+    }
+
+    pub fn parts(&self) -> &[sys::dk_part] {
+        let a = unsafe { &(*self.vtoc).efi_parts }.as_ptr();
+        let n: usize = unsafe { (*self.vtoc).efi_nparts }.try_into().unwrap();
+        unsafe { std::slice::from_raw_parts(a, n) }
+    }
+
+    pub fn parts_mut(&mut self) -> &mut [sys::dk_part] {
+        let a = unsafe { &mut (*self.vtoc).efi_parts }.as_mut_ptr();
+        let n: usize = unsafe { (*self.vtoc).efi_nparts }.try_into().unwrap();
+        unsafe { std::slice::from_raw_parts_mut(a, n) }
+    }
+
+    pub fn lba_first(&self) -> u64 {
+        unsafe { (*self.vtoc).efi_first_u_lba }
+    }
+
+    pub fn lba_last(&self) -> u64 {
+        unsafe { (*self.vtoc).efi_last_u_lba }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::mem::{offset_of, size_of};
+
+    use super::sys::*;
+
+    #[test]
+    fn shapes() {
+        assert_eq!(offset_of!(dk_gpt_t, efi_version), 0x0);
+        assert_eq!(offset_of!(dk_gpt_t, efi_nparts), 0x4);
+        assert_eq!(offset_of!(dk_gpt_t, efi_lbasize), 0xc);
+        assert_eq!(offset_of!(dk_gpt_t, efi_disk_uguid), 0x28);
+        assert_eq!(offset_of!(dk_gpt_t, efi_parts), 0x78);
+        assert_eq!(size_of::<dk_gpt_t>(), 0xf0);
+
+        assert_eq!(offset_of!(dk_part, p_tag), 0x20);
+        assert_eq!(offset_of!(dk_part, p_uguid), 0x48);
+        assert_eq!(size_of::<dk_part>(), 0x78);
+    }
+}
--- factory/gimlet/src/main.rs
+++ factory/gimlet/src/main.rs
@@ -4,18 +4,37 @@
 
 use std::{
     collections::HashMap,
+    os::unix::{fs::OpenOptionsExt, io::AsRawFd},
     path::Path,
     process::{Command, Stdio},
+    time::{Duration, Instant},
 };
 
 use anyhow::{anyhow, bail, Result};
 use debug_parser::ValueKind;
 
 use buildomat_common::*;
+use disks::Slot;
 use humility::{HiffyCaller, PathStep, ValueExt};
 
+mod disks;
+mod efi;
 mod humility;
 mod pipe;
+
+#[derive(Debug, Clone)]
+struct GlobalConfig {
+    humility: String,
+    bootserver: String,
+}
+
+#[derive(Debug, Clone)]
+struct SlotConfig {
+    slot: String,
+    mac: String,
+    ip: String,
+    os: String,
+}
 
 #[derive(Debug)]
 struct System {
@@ -24,12 +43,14 @@ struct System {
     serial: String,
     probe: String,
     archive: String,
+    scfg: SlotConfig,
+    gcfg: GlobalConfig,
 }
 
 impl System {
     fn hiffy(&self, cmd: &str) -> HiffyCaller {
         HiffyCaller {
-            humility: "/usr/bin/humility".to_string(),
+            humility: self.gcfg.humility.to_string(),
             archive: self.archive.to_string(),
             probe: Some(self.probe.to_string()),
             method: cmd.to_string(),
@@ -82,53 +103,18 @@ impl System {
         Ok(())
     }
 
-    /*
-       *   +--> HostFlash.get_mux
-    |       <ok>                        HfMuxState
-    |       <error>                     HfError
-    |
-    +--> HostFlash.set_mux
-    |       state                       HfMuxState
-    |       <ok>                        ()
-    |       <error>                     HfError
-    |
-    +--> HostFlash.get_dev
-    |       <ok>                        HfDevSelect
-    |       <error>                     HfError
-    |
-    +--> HostFlash.set_dev
-    |       dev                         HfDevSelect
-    |       <ok>                        ()
-    |       <error>                     HfError
-    |
-    +--> HostFlash.hash
-    |       address                     u32
-    |       len                         u32
-    |       <ok>                        [u8; drv_hash_api::SHA256_SZ]
-    |       <error>                     HfError
-    |
-    +--> HostFlash.get_persistent_data
-    |       <ok>                        HfPersistentData
-    |       <error>                     HfError
-    |
-    +--> HostFlash.write_persistent_data
-            dev_select                  HfDevSelect
-            <ok>                        ()
-            <error>                     HfError
-      */
-
-    fn write_rom<P: AsRef<Path>>(&self, file: P) -> Result<()> {
-        let file = file.as_ref();
+    fn write_rom(&self) -> Result<()> {
+        let file = format!("{}/rom", self.scfg.os);
         println!("writing rom {file:?}");
 
         let res = Command::new("/usr/bin/pfexec")
             .env_clear()
             .env("HUMILITY_ARCHIVE", &self.archive)
             .env("HUMILITY_PROBE", &self.probe)
-            .arg("/usr/bin/humility")
+            .arg(&self.gcfg.humility)
             .arg("qspi")
             .arg("-D")
-            .arg("/staff/dock/buildomat/gimlet/os/latest/rom")
+            .arg(file)
             .stdout(Stdio::inherit())
             .stdin(Stdio::inherit())
             .stderr(Stdio::inherit())
@@ -141,9 +127,72 @@ impl System {
         Ok(())
     }
 
-    fn open(slot: &str) -> Result<Self> {
+    fn boot_server(&self) -> Result<()> {
+        let file = format!("{}/zfs.img", self.scfg.os);
+        println!("running boot server for {file:?}");
+
+        let res = Command::new("/usr/bin/pfexec")
+            .env_clear()
+            .arg(&self.gcfg.bootserver)
+            .arg("-s")
+            .arg("-t")
+            .arg("300")
+            .arg("e1000g0")
+            .arg(file)
+            .arg(&self.scfg.mac)
+            .stdout(Stdio::inherit())
+            .stdin(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .status()?;
+
+        if !res.success() {
+            bail!("boot server failed");
+        }
+
+        Ok(())
+    }
+
+    fn ssh_options() -> Vec<String> {
+        vec![
+            "StrictHostKeyChecking=no".to_string(),
+            "UserKnownHostsFile=/dev/null".to_string(),
+            "ConnectTimeout=5".to_string(),
+            "LogLevel=error".to_string(),
+            "User=root".to_string(),
+        ]
+    }
+
+    pub fn ssh(&self) -> Command {
+        let mut cmd = Command::new("/usr/bin/ssh");
+        cmd.env_clear();
+
+        for opt in Self::ssh_options() {
+            cmd.arg("-o").arg(opt);
+        }
+
+        cmd.arg(&self.scfg.ip);
+
+        cmd
+    }
+
+    pub fn scp(&self) -> Command {
+        let mut cmd = Command::new("/usr/bin/scp");
+        cmd.env_clear();
+
+        for opt in Self::ssh_options() {
+            cmd.arg("-o").arg(opt);
+        }
+
+        cmd
+    }
+
+    fn open(gcfg: &GlobalConfig, scfg: &SlotConfig) -> Result<Self> {
+        let scfg = scfg.clone();
+        let gcfg = gcfg.clone();
+        let slot = &scfg.slot;
+
         let output =
-            Command::new("/usr/bin/gimlets").env_clear().arg(&slot).output()?;
+            Command::new("/usr/bin/gimlets").env_clear().arg(slot).output()?;
         if !output.status.success() {
             bail!("could not get status for slot {slot}: {}", output.info());
         }
@@ -179,7 +228,7 @@ impl System {
          * Attempt to raise the Gimlet and get its serial number.
          */
         let mut hc = HiffyCaller {
-            humility: "/usr/bin/humility".to_string(),
+            humility: gcfg.humility.to_string(),
             archive: archive.to_string(),
             probe: Some(probe.to_string()),
             method: "ControlPlaneAgent.identity".to_string(),
@@ -215,11 +264,284 @@ impl System {
         )?
         .from_hex()?;
 
-        Ok(System { serial, partno, revision, probe, archive })
+        Ok(System { serial, partno, revision, probe, archive, scfg, gcfg })
     }
 }
 
+fn nvmeadm_format(ns: &str, fmt: Option<&str>) -> Result<()> {
+    let mut cmd = Command::new("/usr/sbin/nvmeadm");
+    cmd.env_clear();
+    cmd.arg("format");
+    cmd.arg(ns);
+    if let Some(fmt) = fmt {
+        cmd.arg(fmt);
+    }
+    let out = cmd.output()?;
+
+    if !out.status.success() {
+        let e = String::from_utf8_lossy(&out.stderr).trim().to_string();
+
+        bail!("nvmeadm format {ns} {fmt:?} failed: {e}");
+    }
+
+    Ok(())
+}
+
+fn cleanup() -> Result<()> {
+    const MAX_TIME: u64 = 60;
+
+    println!(" * formatting disks...");
+    let start = Instant::now();
+    loop {
+        let dur = Instant::now().saturating_duration_since(start);
+        if dur.as_secs() > MAX_TIME {
+            bail!("giving up after {MAX_TIME} seconds");
+        }
+
+        match format_disks() {
+            Ok(()) => {
+                println!(" * disks formatted!");
+                break;
+            }
+            Err(e) => {
+                println!(" * disk format failure: {e}");
+                std::thread::sleep(Duration::from_secs(2));
+                continue;
+            }
+        }
+    }
+
+    println!(" * preparing M.2 disks...");
+    let start = Instant::now();
+    loop {
+        let dur = Instant::now().saturating_duration_since(start);
+        if dur.as_secs() > MAX_TIME {
+            bail!("giving up after {MAX_TIME} seconds");
+        }
+
+        match prepare_m2() {
+            Ok(()) => {
+                println!(" * M.2 disks prepared!");
+                break;
+            }
+            Err(e) => {
+                println!(" * M.2 disk preparation failure: {e}");
+                std::thread::sleep(Duration::from_secs(2));
+                continue;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn prepare_m2() -> Result<()> {
+    let disks = disks::list_disks()?;
+
+    let Some(d) = disks.get(&Slot::M2(0)) else {
+        bail!("could not find the M.2 disk for BSU0");
+    };
+
+    let chr = d.primary().chr.clone();
+    let mut f = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .custom_flags(libc::O_NONBLOCK)
+        .open(&chr)
+        .map_err(|e| anyhow!("open {chr:?} failure: {e}"))?;
+
+    let mut vtoc = efi::Gpt::init_from_fd(f.as_raw_fd())?;
+
+    /*
+     * Knock together a partition table that meets our needs.
+     */
+    let nparts = vtoc.parts().len();
+    let nrs = vtoc.reserved_sectors();
+    let first = vtoc.lba_first();
+    let last = vtoc.lba_last();
+
+    /*
+     * Create the reserved partition in the last slot:
+     */
+    let p = &mut vtoc.parts_mut()[nparts - 1];
+    p.p_tag = efi::sys::V_RESERVED;
+    p.p_start = last - u64::from(nrs) + 1;
+    p.p_size = u64::from(nrs);
+
+    /*
+     * Create the four boot image slots:
+     */
+    let mut next_start = first;
+    let size = (4 * 1024 * 1024 * 1024) / vtoc.block_size();
+    for i in 0usize..4 {
+        let p = &mut vtoc.parts_mut()[i];
+        p.p_tag = efi::sys::V_USR;
+        p.p_start = next_start;
+        p.p_size = size.into();
+        next_start = next_start.checked_add(size).unwrap();
+    }
+
+    /*
+     * Create the dump slice:
+     */
+    let size = (1 * 1024 * 1024 * 1024 * 1024) / vtoc.block_size();
+    let p = &mut vtoc.parts_mut()[4];
+    p.p_tag = efi::sys::V_USR;
+    p.p_start = next_start;
+    p.p_size = size;
+    next_start = next_start.checked_add(size).unwrap();
+
+    /*
+     * Use the remaining space for a ZFS pool slice.
+     */
+    let end = vtoc.parts()[nparts - 1].p_start - 1;
+    let size = end - next_start + 1;
+    let p = &mut vtoc.parts_mut()[5];
+    p.p_tag = efi::sys::V_USR;
+    p.p_start = next_start;
+    p.p_size = size;
+
+    vtoc.write_to_fd(f.as_raw_fd())?;
+
+    /*
+     * Create a plausible facsimile of the BSU pool we generally expect:
+     */
+    let id = uuid::Uuid::new_v4();
+    let pool = format!("oxi_{id}");
+    let out = Command::new("/sbin/zpool")
+        .env_clear()
+        .arg("create")
+        .arg("-O")
+        .arg(&format!("mountpoint=/pool/int/{id}"))
+        .arg("-O")
+        .arg("compress=on")
+        .arg(&pool)
+        .arg(&format!("{}s5", d.name))
+        .output()?;
+
+    if !out.status.success() {
+        let e = String::from_utf8_lossy(&out.stderr).trim().to_string();
+
+        bail!("zpool create failure: {e}");
+    }
+
+    println!(" * zpool {pool} created");
+
+    /*
+     * Update the BSU symlink:
+     */
+    std::fs::create_dir_all("/pool/bsu")?;
+    std::fs::remove_file("/pool/bsu/0").ok();
+    std::os::unix::fs::symlink(&format!("../int/{id}"), "/pool/bsu/0")?;
+
+    /*
+     * Create the swap device:
+     */
+    let out = Command::new("/usr/lib/buildomat/mkswap").env_clear().output()?;
+
+    if !out.status.success() {
+        let e = String::from_utf8_lossy(&out.stderr).trim().to_string();
+
+        bail!("mkswap failure: {e}");
+    }
+
+    println!(" * swap device attached");
+
+    Ok(())
+}
+
+fn format_disks() -> Result<()> {
+    /*
+     * First, list the disks in the system.  We expect there to be two M.2 SSDs
+     * and nine U.2 SSDs.  The tenth U.2 SSD slot contains the K.2 NIC we use to
+     * get the system on the network.
+     */
+    let disks = disks::list_disks()?;
+
+    const WANT: usize = 2 + 9;
+    if disks.len() != WANT {
+        let list = disks
+            .iter()
+            .map(|d| d.slot.label().unwrap_or("?"))
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        bail!("want {WANT} disks, but found {}: {list}", disks.len());
+    }
+
+    /*
+     * Format every disk that we found, so that no detritus from previous jobs
+     * is left behind.
+     */
+    for d in disks {
+        /*
+         * Find the NVMe namespace for this disk.
+         */
+        let out = Command::new("/usr/sbin/nvmeadm")
+            .env_clear()
+            .arg("list")
+            .arg("-p")
+            .arg("-o")
+            .arg("disk,instance,namespace")
+            .output()?;
+
+        if !out.status.success() {
+            let e = String::from_utf8_lossy(&out.stderr).trim().to_string();
+
+            bail!("nvmeadm list failure: {e}");
+        }
+
+        let Some(nvme) = String::from_utf8(out.stdout)?
+            .lines()
+            .filter_map(|l| {
+                let t = l.split(':').collect::<Vec<_>>();
+                if t.len() != 3 {
+                    return None;
+                }
+
+                if t[0] == d.name {
+                    return Some(format!("{}/{}", t[1], t[2]));
+                }
+
+                None
+            })
+            .next()
+        else {
+            bail!("could not locate NVMe namespace for {}", d.name);
+        };
+
+        let label = d.slot.label().unwrap_or("?");
+        match d.slot {
+            Slot::M2(_) => {
+                /*
+                 * We want to create a partition table on the M.2 devices.
+                 * First, format them using the 4K sector format.
+                 *
+                 * XXX The format we're using, "1", depends on a lot of things
+                 * including the drive model and the firmware.  This value is
+                 * only correct for the narrow set of devices we're expecting to
+                 * see in these Gimlet systems.
+                 */
+                println!("M.2 device {label} ({}): {nvme}", d.name);
+                nvmeadm_format(&nvme, Some("1"))?;
+            }
+            Slot::U2(_) => {
+                /*
+                 * Just format the U.2 disks with the default settings and leave
+                 * them blank.
+                 */
+                println!("U.2 device {label} ({}): {nvme}", d.name);
+                //XXX nvmeadm_format(&nvme, None)?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
+    let exe = std::env::current_exe()?;
+
     let a = getopts::Options::new()
         .parsing_style(getopts::ParsingStyle::FloatingFrees)
         .parse(std::env::args_os().skip(1))?;
@@ -228,17 +550,87 @@ fn main() -> Result<()> {
         bail!("which slot?");
     }
 
-    let slot = a.free[0].to_string();
+    if a.free[0] == "cleanup" {
+        return cleanup();
+    }
 
-    let sys = System::open(&slot)?;
+    let gcfg = GlobalConfig {
+        humility: "/usr/bin/humility".to_string(),
+        bootserver: "/staff/bin/bootserver".to_string(),
+    };
+
+    let scfg = SlotConfig {
+        slot: a.free[0].to_string(),
+        mac: "e8:ea:6a:09:7f:6a".to_string(),
+        ip: "172.20.2.180".to_string(),
+        os: "/staff/dock/buildomat/gimlet/os/latest".to_string(),
+    };
+
+    let sys = System::open(&gcfg, &scfg)?;
 
     println!("sys = {sys:#?}");
 
+    let start = Instant::now();
     sys.power_off()?;
     sys.boot_net()?;
     sys.pick_bsu(0)?;
-    sys.write_rom("/staff/dock/buildomat/gimlet/os/latest/rom")?;
+    sys.write_rom()?;
     sys.power_on()?;
+    sys.boot_server()?;
+
+    println!("waiting for system to come up at {}", sys.scfg.ip);
+    let phase = Instant::now();
+    let out = loop {
+        let now = Instant::now();
+
+        let dur = now.saturating_duration_since(phase);
+        if dur.as_secs() > 300 {
+            bail!("gave up after 300 seconds");
+        }
+
+        let out = sys.ssh().arg("pilot local info -j").output()?;
+        if !out.status.success() {
+            std::thread::sleep(Duration::from_secs(1));
+            continue;
+        }
+
+        let Ok(out) = String::from_utf8(out.stdout) else {
+            std::thread::sleep(Duration::from_secs(1));
+            continue;
+        };
+
+        break out;
+    };
+
+    println!("got output: | {out} |");
+
+    let dur = Instant::now().saturating_duration_since(start);
+    println!("duration = {} seconds", dur.as_secs());
+
+    /*
+     * Copy the cleanup program to the remote system.
+     */
+    let out = sys
+        .scp()
+        .arg(&exe)
+        .arg(&format!("{}:/tmp/cleanup", sys.scfg.ip))
+        .output()?;
+    if !out.status.success() {
+        bail!("could not scp {exe:?} to {}", sys.scfg.ip);
+    }
+
+    /*
+     * Run the cleanup program on the remote system.
+     */
+    let out = sys.ssh().arg("/tmp/cleanup cleanup").output()?;
+    if !out.status.success() {
+        let e = String::from_utf8_lossy(&out.stderr).trim().to_string();
+
+        bail!("could not scp {exe:?} to {}: {e}", sys.scfg.ip);
+    }
+
+    let out = String::from_utf8(out.stdout)?;
+    println!("output from cleanup = |{out}|");
 
     Ok(())
 }
